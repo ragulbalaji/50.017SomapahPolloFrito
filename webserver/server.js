fb27e6f33370d9e722430ea88d3a208fcc25a66c
Ragul Balaji <ragulbaby@gmail.com>
2022-07-02 18:34:59 +0800

    basic express

--- webserver/server.js
+++ webserver/server.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+const port = 8443
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Somapah Pollo Frito server running on port ${port}`)
+})
