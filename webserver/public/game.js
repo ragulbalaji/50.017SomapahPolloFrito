1f47e6cf4b755f541a49d11b4e65cc4a8302045f
Ragul Balaji <ragulbaby@gmail.com>
2022-07-14 15:55:20 +0800

    standard style

--- webserver/public/game.js
+++ webserver/public/game.js
@@ -1,213 +1,213 @@
-///////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////////////////////////
 // Constants
 
-const CHUNK_SIZE = 256;
-const CHUNK_SCALE = 1;
-const STEPS_PER_FRAME = 5;
-const GRAVITY = 30;
-const pointerSpeed = 1;
-const _euler = new THREE.Euler(0, 0, 0, "YXZ");
-const playerVelocity = new THREE.Vector3();
-const playerDirection = new THREE.Vector3();
-const playerPosition = new THREE.Vector3(0, 3, 0);
-const clock = new THREE.Clock();
-const _PI_2 = Math.PI / 2;
-const blocker = document.getElementById("blocker");
-const instructions = document.getElementById("instructions");
+const CHUNK_SIZE = 128
+const CHUNK_SCALE = 1
+const STEPS_PER_FRAME = 5
+const GRAVITY = 30
+const pointerSpeed = 2
+const _euler = new THREE.Euler(0, 0, 0, 'YXZ')
+const playerVelocity = new THREE.Vector3()
+const playerDirection = new THREE.Vector3()
+const playerPosition = new THREE.Vector3(0, 3, 0)
+const clock = new THREE.Clock()
+const _PI_2 = Math.PI / 2
+const blocker = document.getElementById('blocker')
+const instructions = document.getElementById('instructions')
 
-const keyStates = {}; // to store key presses
-let playerOnFloor = true;
-let pointerLocked = false;
+const keyStates = {} // to store key presses
+const playerOnFloor = true
+let pointerLocked = false
 
-///////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////////////////////////
 // Set up renderer, scene and camera
 
 const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    precision: "lowp",
-});
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x79a6ff);
+  antialias: true,
+  precision: 'lowp'
+})
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0x79a6ff)
 
 const camera = new THREE.PerspectiveCamera(
-    90,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-camera.rotation.order = "YXZ";
-camera.position.y = 80;
-camera.position.z = -100;
-camera.lookAt(0, 0, 0);
+  90,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+camera.rotation.order = 'YXZ'
+camera.position.y = 80
+camera.position.z = -100
+camera.lookAt(0, 0, 0)
 
-const stats = new Stats();
+const stats = new Stats()
 
-///////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////////////////////////
 // Controls
 
-function getForwardVector() {
-    camera.getWorldDirection(playerDirection);
-    playerDirection.y = 0;
-    playerDirection.normalize();
+function getForwardVector () {
+  camera.getWorldDirection(playerDirection)
+  playerDirection.y = 0
+  playerDirection.normalize()
 
-    return playerDirection;
+  return playerDirection
 }
 
-function getSideVector() {
-    camera.getWorldDirection(playerDirection);
-    playerDirection.y = 0;
-    playerDirection.normalize();
-    playerDirection.cross(camera.up);
+function getSideVector () {
+  camera.getWorldDirection(playerDirection)
+  playerDirection.y = 0
+  playerDirection.normalize()
+  playerDirection.cross(camera.up)
 
-    return playerDirection;
+  return playerDirection
 }
 
-function controls(deltaTime) {
-    let speedDelta = deltaTime * (playerOnFloor ? 25 : 15);
+function controls (deltaTime) {
+  let speedDelta = deltaTime * (playerOnFloor ? 100 : 25)
 
-    // if shift is pressed then move at triple speed
-    if (keyStates["ShiftLeft"] || keyStates["ShiftRight"]) {
-        speedDelta *= 3;
+  // if shift is pressed then move at triple speed
+  if (keyStates.ShiftLeft || keyStates.ShiftRight) {
+    speedDelta *= 3
+  }
+  if (keyStates.KeyW || keyStates.ArrowUp) {
+    // forward
+    playerVelocity.add(getForwardVector().multiplyScalar(speedDelta))
+  }
+  if (keyStates.KeyS || keyStates.ArrowDown) {
+    // backward
+    playerVelocity.add(getForwardVector().multiplyScalar(-speedDelta))
+  }
+  if (keyStates.KeyA || keyStates.ArrowLeft) {
+    // left
+    playerVelocity.add(getSideVector().multiplyScalar(-speedDelta))
+  }
+  if (keyStates.KeyD || keyStates.ArrowRight) {
+    // right
+    playerVelocity.add(getSideVector().multiplyScalar(speedDelta))
+  }
+  if (keyStates.KeyQ || keyStates.KeyZ) {
+    // fly up
+    playerVelocity.y += speedDelta
+  }
+  if (keyStates.KeyE || keyStates.KeyX) {
+    // fly down
+    playerVelocity.y -= speedDelta
+  }
+  if (playerOnFloor) {
+    // jump
+    if (keyStates.Space) {
+      playerVelocity.y = 15
     }
-    if (keyStates["KeyW"] || keyStates["ArrowUp"]) {
-        // forward
-        playerVelocity.add(getForwardVector().multiplyScalar(speedDelta));
-    }
-    if (keyStates["KeyS"] || keyStates["ArrowDown"]) {
-        // backward
-        playerVelocity.add(getForwardVector().multiplyScalar(-speedDelta));
-    }
-    if (keyStates["KeyA"] || keyStates["ArrowLeft"]) {
-        // left
-        playerVelocity.add(getSideVector().multiplyScalar(-speedDelta));
-    }
-    if (keyStates["KeyD"] || keyStates["ArrowRight"]) {
-        // right
-        playerVelocity.add(getSideVector().multiplyScalar(speedDelta));
-    }
-    if (keyStates["KeyQ"] || keyStates["KeyZ"]) {
-        // fly up
-        playerVelocity.y += speedDelta;
-    }
-    if (keyStates["KeyE"] || keyStates["KeyX"]) {
-        // fly down
-        playerVelocity.y -= speedDelta;
-    }
-    if (playerOnFloor) {
-        // jump
-        if (keyStates["Space"]) {
-            playerVelocity.y = 15;
-        }
-    }
+  }
 }
 
-function updatePlayer(deltaTime) {
-    let damping = Math.exp(-4 * deltaTime) - 1;
+function updatePlayer (deltaTime) {
+  let damping = Math.exp(-4 * deltaTime) - 1
 
-    if (!playerOnFloor) {
-        playerVelocity.y -= GRAVITY * deltaTime;
-        // small air resistance
-        damping *= 0.1;
-    }
+  if (!playerOnFloor) {
+    playerVelocity.y -= GRAVITY * deltaTime
+    // small air resistance
+    damping *= 0.1
+  }
 
-    playerVelocity.addScaledVector(playerVelocity, damping);
+  playerVelocity.addScaledVector(playerVelocity, damping)
 
-    const deltaPosition = playerVelocity.clone().multiplyScalar(deltaTime);
+  const deltaPosition = playerVelocity.clone().multiplyScalar(deltaTime)
 
-    playerPosition.add(deltaPosition);
+  playerPosition.add(deltaPosition)
 
-    camera.position.copy(playerPosition);
+  camera.position.copy(playerPosition)
 }
 
-function onPointerLockChange() {
-    if (document.pointerLockElement === document.body) {
-        instructions.style.display = "none";
-        blocker.style.display = "none";
-        pointerLocked = true;
-    } else {
-        pointerLocked = false;
-        blocker.style.display = "block";
-        instructions.style.display = "";
-    }
+function onPointerLockChange () {
+  if (document.pointerLockElement === document.body) {
+    instructions.style.display = 'none'
+    blocker.style.display = 'none'
+    pointerLocked = true
+  } else {
+    pointerLocked = false
+    blocker.style.display = 'block'
+    instructions.style.display = ''
+  }
 }
 
-function onMouseMovement(e) {
-    if (!pointerLocked) return;
+function onMouseMovement (e) {
+  if (!pointerLocked) return
 
-    const movementX = e.movementX || e.mozMovementX || e.webkitMovementX || 0;
-    const movementY = e.movementY || e.mozMovementY || e.webkitMovementY || 0;
+  const movementX = e.movementX || e.mozMovementX || e.webkitMovementX || 0
+  const movementY = e.movementY || e.mozMovementY || e.webkitMovementY || 0
 
-    _euler.setFromQuaternion(camera.quaternion);
+  _euler.setFromQuaternion(camera.quaternion)
 
-    _euler.y -= movementX * 0.002 * pointerSpeed;
-    _euler.x -= movementY * 0.002 * pointerSpeed;
+  _euler.y -= movementX * 0.002 * pointerSpeed
+  _euler.x -= movementY * 0.002 * pointerSpeed
 
-    const minPolarAngle = 0; // radians
-    const maxPolarAngle = Math.PI; // radians
+  const minPolarAngle = 0 // radians
+  const maxPolarAngle = Math.PI // radians
 
-    _euler.x = Math.max(
-        _PI_2 - maxPolarAngle,
-        Math.min(_PI_2 - minPolarAngle, _euler.x)
-    );
+  _euler.x = Math.max(
+    _PI_2 - maxPolarAngle,
+    Math.min(_PI_2 - minPolarAngle, _euler.x)
+  )
 
-    camera.quaternion.setFromEuler(_euler);
+  camera.quaternion.setFromEuler(_euler)
 }
 
-///////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////////////////////////
 // Helpers
 
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+function onWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
-///////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////////////////////////
 // Initialize
 
-init();
+init()
 
-function init() {
-    stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-    document.body.appendChild(stats.dom);
+function init () {
+  stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.body.appendChild(stats.dom)
 
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
 
-    window.addEventListener("resize", onWindowResize);
+  window.addEventListener('resize', onWindowResize)
 
-    instructions.addEventListener("click", function () {
-        document.body.requestPointerLock =
+  instructions.addEventListener('click', function () {
+    document.body.requestPointerLock =
             document.body.requestPointerLock ||
-            document.body.mozRequestPointerLock;
-        document.body.requestPointerLock();
-    });
+            document.body.mozRequestPointerLock
+    document.body.requestPointerLock()
+  })
 
-    document.body.addEventListener("mousemove", onMouseMovement);
-    document.addEventListener("pointerlockchange", onPointerLockChange);
+  document.body.addEventListener('mousemove', onMouseMovement)
+  document.addEventListener('pointerlockchange', onPointerLockChange)
 
-    document.addEventListener("keydown", (e) => {
-        keyStates[e.code] = true;
-    });
-    document.addEventListener("keyup", (e) => {
-        keyStates[e.code] = false;
-    });
+  document.addEventListener('keydown', (e) => {
+    keyStates[e.code] = true
+  })
+  document.addEventListener('keyup', (e) => {
+    keyStates[e.code] = false
+  })
 }
 
-///////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////////////////////////
 // Geometry
 
 const geometry = new THREE.PlaneGeometry(
-    CHUNK_SIZE * CHUNK_SCALE,
-    CHUNK_SIZE * CHUNK_SCALE,
-    CHUNK_SIZE - 1,
-    CHUNK_SIZE - 1
-);
-geometry.rotateX(-Math.PI / 2);
+  CHUNK_SIZE * CHUNK_SCALE,
+  CHUNK_SIZE * CHUNK_SCALE,
+  CHUNK_SIZE - 1,
+  CHUNK_SIZE - 1
+)
+geometry.rotateX(-Math.PI / 2)
 
 const material = new THREE.ShaderMaterial({
-    vertexShader: `
+  vertexShader: `
   varying vec3 vPosition;
   varying vec2 vUV;
   varying float sea;
@@ -223,7 +223,7 @@ const material = new THREE.ShaderMaterial({
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   }
   `,
-    fragmentShader: `
+  fragmentShader: `
   varying vec3 vPosition;
   varying vec2 vUV;
   varying float sea;
@@ -253,19 +253,19 @@ const material = new THREE.ShaderMaterial({
     }
     
   }
-  `,
-});
+  `
+})
 
 const phongmaterial = new THREE.ShaderMaterial({
-    uniforms: {
-        Ka: { value: new THREE.Vector3(0.4, 0.9, 0.3) },
-        Kd: { value: new THREE.Vector3(0.4, 0.9, 0.3) },
-        Ks: { value: new THREE.Vector3(0.8, 0.8, 0.8) },
-        LightIntensity: { value: new THREE.Vector4(0.5, 0.5, 0.5, 1.0) },
-        LightPosition: { value: new THREE.Vector4(0.0, 2000.0, 0.0, 1.0) },
-        Shininess: { value: 10.0 },
-    },
-    vertexShader: `
+  uniforms: {
+    Ka: { value: new THREE.Vector3(0.4, 0.9, 0.3) },
+    Kd: { value: new THREE.Vector3(0.4, 0.9, 0.3) },
+    Ks: { value: new THREE.Vector3(0.8, 0.8, 0.8) },
+    LightIntensity: { value: new THREE.Vector4(0.5, 0.5, 0.5, 1.0) },
+    LightPosition: { value: new THREE.Vector4(0.0, 2000.0, 0.0, 1.0) },
+    Shininess: { value: 10.0 }
+  },
+  vertexShader: `
     varying vec3 Normal;
     varying vec3 Position;
 
@@ -275,7 +275,7 @@ const phongmaterial = new THREE.ShaderMaterial({
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
   `,
-    fragmentShader: `
+  fragmentShader: `
     varying vec3 Normal;
     varying vec3 Position;
 
@@ -302,82 +302,79 @@ const phongmaterial = new THREE.ShaderMaterial({
     void main() {
       gl_FragColor = vec4(phong(), 1.0);
     }
-  `,
-});
+  `
+})
 
-const material2 = new THREE.MeshNormalMaterial({ flatShading: true });
-const chunk = new THREE.Mesh(geometry, material);
-scene.add(chunk);
+const material2 = new THREE.MeshNormalMaterial({ flatShading: true })
+const chunk = new THREE.Mesh(geometry, material)
+scene.add(chunk)
 
-let maxh = 0;
-makeChunk(0, 0);
-function makeChunk(x0, y0) {
-    const vertices = chunk.geometry.attributes.position.array;
-    const uv = chunk.geometry.attributes.uv.array;
-    maxh = 8;
-    const seed = 1;
-    noise.seed(seed);
-    for (let y = 0; y < CHUNK_SIZE; y++) {
-        for (let x = 0; x < CHUNK_SIZE; x++) {
-            const j = 2 * (y * CHUNK_SIZE + x);
-            const temp =
-                (noise.simplex2((x0 + x) / 512, (y0 + y) / 512) + 1) / 2;
-            let rain = (noise.simplex2((x0 + x) / 256, (y0 + y) / 256) + 1) / 2;
-            rain = Math.min(rain, 1 - temp);
-            uv[j] = temp;
-            uv[j + 1] = rain;
+let maxh = 0
+makeChunk(0, 0)
+function makeChunk (x0, y0) {
+  const vertices = chunk.geometry.attributes.position.array
+  const uv = chunk.geometry.attributes.uv.array
+  maxh = 8
+  const seed = 1
+  noise.seed(seed)
+  for (let y = 0; y < CHUNK_SIZE; y++) {
+    for (let x = 0; x < CHUNK_SIZE; x++) {
+      const j = 2 * (y * CHUNK_SIZE + x)
+      const temp =
+                (noise.simplex2((x0 + x) / 512, (y0 + y) / 512) + 1) / 2
+      let rain = (noise.simplex2((x0 + x) / 256, (y0 + y) / 256) + 1) / 2
+      rain = Math.min(rain, 1 - temp)
+      uv[j] = temp
+      uv[j + 1] = rain
 
-            const i = 3 * (y * CHUNK_SIZE + x);
-            const h =
+      const i = 3 * (y * CHUNK_SIZE + x)
+      const h =
                 noise.simplex2((x0 + x) / 4, (y0 + y) / 4) * (rain + 0.3) +
                 noise.simplex2((x0 + x) / 128, (y0 + y) / 128) * 4 +
                 Math.max(
-                    0,
-                    noise.simplex2((x0 + x) / 1024, (y0 + y) / 1024) * 32
-                );
-            const r =
+                  0,
+                  noise.simplex2((x0 + x) / 1024, (y0 + y) / 1024) * 32
+                )
+      const r =
                 Math.abs(noise.simplex2((x0 + x) / 96, (y0 + y) / 96)) <
-                Math.min(0.2, Math.abs((3 - h) / 8));
-            vertices[i + 1] = Math.max(0, h);
-            if (h < 0) {
-                uv[j] = 1;
-                uv[j + 1] = 1;
-            }
-            if (r && vertices[i + 1] < 3) {
-                vertices[i + 1] *= 0.2;
-                uv[j] = 1;
-                uv[j + 1] = 1;
-            }
-            maxh = Math.max(maxh, vertices[i + 1]);
-        }
+                Math.min(0.2, Math.abs((3 - h) / 8))
+      vertices[i + 1] = Math.max(0, h)
+      if (h < 0) {
+        uv[j] = 1
+        uv[j + 1] = 1
+      }
+      if (r && vertices[i + 1] < 3) {
+        vertices[i + 1] *= 0.2
+        uv[j] = 1
+        uv[j + 1] = 1
+      }
+      maxh = Math.max(maxh, vertices[i + 1])
     }
+  }
 
-    chunk.geometry.attributes.position.needsUpdate = true;
-    chunk.geometry.attributes.uv.needsUpdate = true;
+  chunk.geometry.attributes.position.needsUpdate = true
+  chunk.geometry.attributes.uv.needsUpdate = true
 }
 
-///////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////////////////////////
 // Animate
 
-let xxx = 0;
-makeChunk(0, xxx);
+const xxx = 0
+makeChunk(0, xxx)
 
-function animate() {
-    const deltaTime = Math.min(0.05, clock.getDelta()) / STEPS_PER_FRAME;
+function animate () {
+  const deltaTime = Math.min(0.05, clock.getDelta()) / STEPS_PER_FRAME
 
-    for (let i = 0; i < STEPS_PER_FRAME; i++) {
-        controls(deltaTime);
-        updatePlayer(deltaTime);
-    }
+  for (let i = 0; i < STEPS_PER_FRAME; i++) {
+    controls(deltaTime)
+    updatePlayer(deltaTime)
+  }
 
-    
-    // xxx += 1;
+  renderer.render(scene, camera)
 
-    renderer.render(scene, camera);
+  stats.update()
 
-    stats.update();
-
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate)
 }
 
-animate();
+animate()
