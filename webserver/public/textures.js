81c6cb015a4ae2f76cbb1f761ae73fada744a1be
Ragul Balaji <ragulbaby@gmail.com>
2022-07-18 01:57:57 +0800

    Update textures.js

--- webserver/public/textures.js
+++ webserver/public/textures.js
@@ -2,5 +2,6 @@ const texture_loader = new THREE.TextureLoader()
 
 const TEXTURES = {
   tex_grass: texture_loader.load('assets/tex/grass.png'),
-  tex_sky: texture_loader.load('assets/tex/sky.jpg')
+  tex_sky: texture_loader.load('assets/tex/sky.jpg'),
+  tex_test: texture_loader.load('assets/tex/texmap.png')
 }
