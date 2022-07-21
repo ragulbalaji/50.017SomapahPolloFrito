8932f51b0ca3e4b806c06c3259f220e325b44b41
James R T <jamestiotio@gmail.com>
2022-07-21 20:01:29 +0800

    Yeet unused material

--- webserver/public/materials.js
+++ webserver/public/materials.js
@@ -1,41 +1,11 @@
-const material = new THREE.ShaderMaterial({
-  vertexShader: `varying vec3 vPosition;
-  varying vec2 vUV;
-  void main() {
-    vPosition = position;
-    vUV = uv;
-    
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  }`,
-  fragmentShader: `varying vec3 vPosition;
-  varying vec2 vUV;
-  uniform sampler2D uTexture;
-
-  void main() {
-    if (vUV.x > 0.8 && vUV.y > 0.8) {
-      gl_FragColor = vec4(0.1, 0.2, 0.7 - vPosition.y, 1.0);
-    } else {
-      vec3 color = texture2D(uTexture, vUV).rgb;
-      gl_FragColor = vec4(color, 1.0);
-      // gl_FragColor = vec4(vUV.x, vUV.y, 0.0, 1.0);
-    }
-    
-  }`,
-  uniforms: {
-    uTexture: {
-      value: TEXTURES.tex_grass
-    }
-  }
-})
-
-const phong_material = new THREE.MeshPhongMaterial({
+const material = new THREE.MeshPhongMaterial({
   color: 0xfdfbd3,
   shininess: 10,
   flatShading: true,
   map: TEXTURES.tex_HandM
 })
 
-phong_material.onBeforeCompile = function (materialInfo) {
+material.onBeforeCompile = function (materialInfo) {
   materialInfo.vertexUvs = true
   materialInfo.uvsVertexOnly = false
   materialInfo.vertexShader = materialInfo.vertexShader.replace(
@@ -71,6 +41,5 @@ phong_material.onBeforeCompile = function (materialInfo) {
 
 const MATERIALS = {
   material,
-  phong_material,
   normal_material: new THREE.MeshNormalMaterial({ flatShading: true })
 }
