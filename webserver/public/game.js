3651bb975af833532e3fe714fb0f3522979bfb90
James R T <jamestiotio@gmail.com>
2022-08-04 17:05:48 +0800

    Clean up

--- webserver/public/game.js
+++ webserver/public/game.js
@@ -552,11 +552,7 @@ function updateDayNight () {
   }
   PARAMETERS.directional_light_angle %= 180
   directionalLight.position.set(100, 100, -100 / Math.tan(PARAMETERS.directional_light_angle * Math.PI / 180))
-  const darkeningFactor = (-Math.sin(PARAMETERS.directional_light_angle * Math.PI / 180) + 1) * 100
-  const currentSkyColor = tinycolor('#' + PARAMETERS.sky_color.toString(16))
-  const newSkyColor = parseInt(currentSkyColor.darken(darkeningFactor).toString().substring(1), 16)
-  scene.background = new THREE.Color(newSkyColor)
-  scene.fog = new THREE.FogExp2(newSkyColor, PARAMETERS.fog_density)
+  updateNightSky()
 }
 
 function updateNightSky () {
